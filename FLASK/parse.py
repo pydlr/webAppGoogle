44cae4ce62600b8503cb1bedf4336733368b4269
Roberto <io_robertob@hotmail.com>
2017-11-03 19:17:27 +0200

    parse dev

--- FLASK/parse.py
+++ FLASK/parse.py
@@ -43,34 +43,34 @@ def main():
 	DB_ENTRIES[i] = AUTORIDAD
 	print 'Autoridad: ' + str(AUTORIDAD)
 
-	scanHTML(firstElement , i+1)
+	next_element = firstElement.find_next('span')
+
+	scanHTML(next_element , i+1)
 # ======================================================================
 
 
 # ======================================================================
 # i = El numero de columna en la base de datos
-def scanHTML(prev_element, i ):
+def scanHTML(next_element, i ):
 	finished = False
+	alreadyIncremented = False
+	prev_element = next_element;
 	while not finished:
-		# print 'before'
-		# print prev_element 
-		next_element = prev_element.find_next('span')
+
+		# next_element = prev_element.find_next('span')
+
 		if next_element.text.find('EDICTO') != -1:
 			print 'finishing'
 			print 'Edicto: ' + str(next_element.text.find('EDICTO'))
 			finished = True
-		# print 'after'
-		# print 'i: ' + str(i)
-		# print levelOptions[i]
 		for option in levelOptions[i]:
-			# if option.find(str(next_element.text.strip())) != -1:
-			# print next_element.text
+
 			if next_element.text.find(option) != -1:
 
-				# print 'in for cycle: '
-				# print next_element.text.strip()
 				DB_ENTRIES[i] = next_element.text.strip()
+				print DB_ENTRIES[i]
 
+				# Tabla
 				if i == 2:
 					nextd = next_element.find_next('td')
 
@@ -89,62 +89,88 @@ def scanHTML(prev_element, i ):
 						DB_ENTRIES[i+3] = nextd.text.strip()
 
 						print 'DB: ' + str(DB_ENTRIES)
-
 						next_element = nextd
 
 						# Find out if the table continues
 						nextd = next_element.find_next().find_next().find_next().find_next().find_next().find_next()
 
-					# scanHTML(next_element, 0)
-					
-				# scanHTML( next_element, i+1)
-				i += 1
-				if i > 2:
-					i = 2
-				# print 'new i: '  + str(i)
+				next_element = next_element.find_next('span')
+				alreadyIncremented = True
+				# if i <= 2:
+				# 	i += 1
+				# else:
+				# 	i = 2 
+				# 	next_element = prev_element.find_next('span')
 
-				break
+			next_element = next_element.find_next('span')
+			alreadyIncremented = True
+			break
+
+		if i < 2:
+			i += 1
+		else:
+			i = 0 
+			if not alreadyIncremented:
+				next_element = next_element.find_next('span')
+				alreadyIncremented = False
+
+
+
 		prev_element = next_element
-		# scanHTML(next_element,i
 
 
 
+# # ======================================================================
+# # i = El numero de columna en la base de datos
+# def scanHTML(prev_element, i ):
+# 	finished = False
+# 	while not finished:
+		
+# 		
+# 		next_element = prev_element.find_next('span')
+# 		if next_element.text.find('EDICTO') != -1:
+# 			print 'finishing'
+# 			print 'Edicto: ' + str(next_element.text.find('EDICTO'))
+# 			finished = True
+
+# 		for option in levelOptions[i]:
+
+# 			if next_element.text.find(option) != -1:
+
+# 				DB_ENTRIES[i] = next_element.text.strip()
+
+# 				if i == 2:
+# 					nextd = next_element.find_next('td')
+
+# 					while nextd.name.find('td') != -1:
+
+# 						# NUMERO DE CASO EN LA HOJA 
+# 						DB_ENTRIES[i+1] = nextd.text.strip()
+
+# 						# NUMERO DE EXPEDIENTE
+# 						nextd = nextd.find_next('td')
+# 						DB_ENTRIES[i+2] = nextd.text.strip()
+
+
+# 						# TEXTO DE LA RESOLUCION
+# 						nextd = nextd.find_next('td')
+# 						DB_ENTRIES[i+3] = nextd.text.strip()
+
+# 						print 'DB: ' + str(DB_ENTRIES)
+# 						next_element = nextd
+
+# 						# Find out if the table continues
+# 						nextd = next_element.find_next().find_next().find_next().find_next().find_next().find_next()
+
+# 				i += 1
+# 				if i > 2:
+# 					i = 2
+
+# 				break
+# 		prev_element = next_element
 
 
 
-
-
-
-
-
-
-
-	# for option in levelOptions[i]:
-	# 	print 'OPTION: ' +  str(option)
-
-
-	# finished = False
-	# previous = prev_element
-	# while not finished:
-	# 	next = previous.find_next('span')
-
-	# 	if next == 'EDICTO':
-	# 		finished = True 
-
-	# 	for option in levelOptions[i]:
-
-	# 		if option in next:
-				
-	# 			levelNames[i] = next
-	# 			i =+ 1
-
-	# 			if i >= 5:
-	# 				i = 3
-
-
-	# 			break
-	# 	previous = next
-# ======================================================================
 
 
 if __name__ == "__main__":
