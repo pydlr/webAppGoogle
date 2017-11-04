a97566d0c960879c78e4155ff065bff608623816
Roberto <io_robertob@hotmail.com>
2017-11-04 19:57:04 +0200

    parse dev

--- FLASK/parse.py
+++ FLASK/parse.py
@@ -24,7 +24,7 @@ levelOptions[2] = [ 'Acuerdos',
 					'Sentencias',
 					'Exhortos',
 					'Cuadernos de Amparo',
-					'Cuadernos de Antecedentes']
+					'Cuadernos De Antecedentes']
 
 # ======================================================================
 def main():
@@ -50,7 +50,6 @@ def main():
 	scanHTML(next_element , i+1)
 # ======================================================================
 
-
 # ======================================================================
 # i = El numero de columna en la base de datos
 def scanHTML(next_element, i ):
@@ -67,24 +66,39 @@ def scanHTML(next_element, i ):
 		# =======EXIT CONDITION=======
 		if next_element.text.find('EDICTO') != -1:
 			print 'finishing'
-			print 'Edicto: ' + str(next_element.text.find('EDICTO'))
+			# print 'Edicto: ' + str(next_element.text.find('EDICTO'))
 			finished = True
 		#==============================
 
-		# print next_element
+		# =======EXIT CONDITION=======
+		# if next_element.text.find('MARIA DOLORES MORENO ROMERO') != -1:
+		# 	print 'finishing'
+		# 	# print 'Edicto: ' + str(next_element.text.find('EDICTO'))
+		# 	finished = True
+		#============================== 
+		
 
+		# print next_element
+		# print str(i) + 'looking for: ' + str(next_element)
 		for option in levelOptions[i]:
 
 			if next_element.text.find(option) != -1:
 
 				DB_ENTRIES[i] = next_element.text.strip()
-				print DB_ENTRIES[i]
+				print str(i) + ' : Entry : ' + str(DB_ENTRIES[i].encode('utf-8'))
 
 				# Tabla
 				if i == 2:
 					nextd = next_element.find_next('td')
+					# print 'find_next'
 
-					while nextd.name.find('td') != -1:
+					while  ((nextd.name.find('td') != -1) or
+							(nextd.name.find('tr') != -1)):
+
+						if (nextd.name.find('tr') != -1):
+							# print 'here'
+							nextd = nextd.find_next('td')
+							# print nextd
 
 						# NUMERO DE CASO EN LA HOJA 
 						DB_ENTRIES[i+1] = nextd.text.strip()
@@ -97,18 +111,25 @@ def scanHTML(next_element, i ):
 						nextd = nextd.find_next('td')
 						DB_ENTRIES[i+3] = nextd.text.strip()
 
-						print 'DB: ' + str(DB_ENTRIES)
+						print str(i) + ': DB: ' + str(DB_ENTRIES)
 						next_element = nextd
+						# print next_element.name
 
 						# Find out if the table continues
-						nextd = next_element.find_next().find_next().find_next().find_next().find_next()
-						i = 0;
+						nextd = nextd.find_next().find_next().find_next().find_next().find_next()
+						
+						# print 'nextd'
 						# print nextd
+						
+					i = 0; 
+				# print str(i) + ' : AFTER WHILE : ' + str(next_element)
 
+				# print str(i) + 'pre-break: ' + str(next_element)
 				next_element = next_element.find_next('span')
 				i = 0
 				#print next_element
 				alreadyIncremented = True
+				# print str(i) +  ' : break: ' + str(next_element)
 				break
 				# if i <= 2:
 				# 	i += 1
@@ -116,9 +137,10 @@ def scanHTML(next_element, i ):
 				# 	i = 2 
 				# 	next_element = prev_element.find_next('span')
 
-			if not alreadyIncremented:
-				next_element = next_element.find_next('span')
-				alreadyIncremented = True
+			# if not alreadyIncremented:# CREO QUE ESTE FIND NEXT NO DEBERIA ESTAR AQUI, SOLO CUANDO SE LLEGA A 3 SE INCREMENTA
+			# 	next_element = next_element.find_next('span')
+			# 	print str(i) + 'next_element: ' + str(next_element)
+			# 	alreadyIncremented = True
 			# Not finding next because we did not find nothing
 			# next_element = next_element.find_next('span')
 			# alreadyIncremented = True
@@ -129,8 +151,15 @@ def scanHTML(next_element, i ):
 
 		i += 1
 		if i >2:
+			
 			i = 0
+			# if not incremented yet!!!!!! TODO
 			next_element = next_element.find_next('span')
+			# print str(i) +  ' : past_3: ' + str(next_element)
+			# print 'blah' + str(next_element.text.find(levelOptions[2][6]))
+
+			# exit()
+
 		# if not alreadyIncremented:
 		# 	if i < 2:
 		# 		i += 1
